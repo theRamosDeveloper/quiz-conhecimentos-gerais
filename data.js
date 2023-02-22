efde001ee2f98fbb3066f8da8d86cfbcd64957f7
theRamosDeveloper <104318910+theRamosDeveloper@users.noreply.github.com>
2023-02-22 10:05:50 -0300

    Quiz - HTML | CSS | JS

---
+++ data.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      question: "Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?",
+      answers: [
+        { option: "Tem entre 2 a 4 litros. São retirados 450 mililitros", correct: false },
+        { option: "Tem entre 4 a 6 litros. São retirados 450 mililitros", correct: true },
+        { option: "Tem 7 litros. São retirados 1,5 litros", correct: false },
+        { option: "Tem 0,5 litros. São retirados 0,5 litros", correct: false },
+      ],
+    },
+    {
+      question: "De quem é a famosa frase “Penso, logo existo”?",
+      answers: [
+        { option: "Platão", correct: false },
+        { option: "Galileu Galilei", correct: false },
+        { option: "Descartes", correct: true },
+        { option: "Sócrates", correct: false },
+      ],
+    },
+    {
+      question: "De onde é a invenção do chuveiro elétrico?",
+      answers: [
+        { option: "Brasil", correct: true },
+        { option: "França", correct: false },
+        { option: "Inglaterra", correct: false },
+        { option: "Alemanha", correct: false },
+      ],
+    },
+    {
+      question: "Quais o menor e o maior país do mundo?",
+      answers: [
+        { option: "Vaticano e Rússia", correct: true },
+        { option: "Mônaco e Canadá", correct: false },
+        { option: "Malta e Estados Unidos", correct: false },
+        { option: "Nauru e China", correct: false },
+      ],
+    },
+    {
+      question: "Qual o nome do presidente do Brasil que ficou conhecido como Jango?",
+      answers: [
+        { option: "Jânio Quadros", correct: false },
+        { option: "João Figueiredo", correct: false },
+        { option: "João Goulart", correct: true },
+        { option: "Jacinto Anjos", correct: false },
+      ],
+    },
+    {
+      question: "Qual o grupo em que todas as palavras foram escritas corretamente?",
+      answers: [
+        { option: "Asterisco, beneficente, meteorologia, entretido", correct: true },
+        { option: "Asterístico, beneficiente, meteorologia, entertido", correct: false },
+        { option: "Asterisco, beneficente, metereologia, entretido", correct: false },
+        { option: "Asterisco, beneficiente, metereologia, entretido", correct: false },
+      ],
+    },
+    {
+      question: "Atualmente, quantos elementos químicos a tabela periódica possui?",
+      answers: [
+        { option: "113", correct: false },
+        { option: "109", correct: false },
+        { option: "120", correct: false },
+        { option: "118", correct: true },
+      ],
+    },
+    {
+      question: "O que a palavra legend significa em português?",
+      answers: [
+        { option: "Legenda", correct: false },
+        { option: "Lengendário", correct: false },
+        { option: "Legião", correct: false },
+        { option: "Lenda", correct: true },
+      ],
+    },
+    {
+      question: "Qual o número mínimo de jogadores em cada time numa partida de futebol?",
+      answers: [
+        { option: "8", correct: false },
+        { option: "7", correct: true },
+        { option: "6", correct: false },
+        { option: "9", correct: false },
+      ],
+    },
+    {
+      question: "Quanto tempo a luz do Sol demora para chegar à Terra?",
+      answers: [
+        { option: "1 hora", correct: false },
+        { option: "5 minutos", correct: false },
+        { option: "12 minutos", correct: false },
+        { option: "8 minutos", correct: true },
+      ],
+    },
+  ];
